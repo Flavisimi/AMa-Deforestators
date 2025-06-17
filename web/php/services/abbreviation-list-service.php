286a187eb3c0baa1bcfd28a13c0df333a6da3c5c
unknown-characteristics <78977619+unknown-characteristics@users.noreply.github.com>
2025-06-17 23:44:13 +0300

    load meaning from abbreviation docbook file

--- web/php/services/abbreviation-list-service.php
+++ web/php/services/abbreviation-list-service.php
@@ -2,6 +2,8 @@
 
 namespace ama\services;
 
+use ama\repositories\AbbreviationRepository;
+
 require_once(__DIR__ . "/../models/abbreviation-list.php");
 require_once(__DIR__ . "/../repositories/meaning-repository.php");
 
@@ -13,6 +15,11 @@ class AbbreviationListService
     public static function attach_meanings($conn, AbbreviationList & $abbr_list)
     {
         $meanings = MeaningRepository::load_meanings_from_list($conn, $abbr_list->id);
+        foreach($meanings as $meaning)
+        {
+            $abbreviation = AbbreviationRepository::load_abbreviation($conn, $meaning->abbr_id);
+            MeaningService::attach_description($meaning, $abbreviation->searchable_name);
+        }
         $abbr_list->meanings = $meanings;
     }
 }

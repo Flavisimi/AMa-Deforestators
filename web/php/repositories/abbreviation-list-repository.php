690a7c25f3287bc3bc3e975cb846d6b9ef0503ea
unknown-characteristics <78977619+unknown-characteristics@users.noreply.github.com>
2025-06-25 21:08:41 +0300

    view creator username for abbreviation lists

--- web/php/repositories/abbreviation-list-repository.php
+++ web/php/repositories/abbreviation-list-repository.php
@@ -13,6 +13,8 @@ error_reporting(0);
 
 class AbbreviationListRepository
 {
+    public static string $SQL_SELECT_TEMPLATE = "select id, creator_id, name, private, created_at, updated_at, (select name from users where id = abbr_lists.creator_id) as creator_name from abbr_lists";
+
     public static function convert_row_to_object( $row ): AbbreviationList
     {
         $abbr_list = new AbbreviationList();
@@ -24,13 +26,14 @@ class AbbreviationListRepository
         $abbr_list->created_at->setTimestamp(strtotime($row["CREATED_AT"]));
         $abbr_list->updated_at = new \DateTime();
         $abbr_list->updated_at->setTimestamp(strtotime($row["UPDATED_AT"]));
+        $abbr_list->creator_name = $row["CREATOR_NAME"];
 
         return $abbr_list;
     }
 
     public static function load_abbreviation_list($conn, int $id): ?AbbreviationList
     {
-        $stmt = oci_parse($conn, "select id, creator_id, name, private, created_at, updated_at from abbr_lists where id = :id");
+        $stmt = oci_parse($conn, self::$SQL_SELECT_TEMPLATE . " where id = :id");
         if(!$stmt) 
             throw new ApiException(500, "Failed to parse SQL statement");
 
@@ -55,7 +58,7 @@ class AbbreviationListRepository
 
     public static function load_all_abbr_lists($conn): ?array
     {
-        $stmt = oci_parse($conn, "select id, creator_id, name, private, created_at, updated_at from abbr_lists");
+        $stmt = oci_parse($conn, self::$SQL_SELECT_TEMPLATE);
         if(!$stmt) 
             throw new ApiException(500, "Failed to parse SQL statement");
 
@@ -77,7 +80,7 @@ class AbbreviationListRepository
 
     public static function load_all_abbr_lists_by_user($conn, int $user_id): ?array
     {
-        $stmt = oci_parse($conn, "select id, creator_id, name, private, created_at, updated_at from abbr_lists where creator_id = :id");
+        $stmt = oci_parse($conn, self::$SQL_SELECT_TEMPLATE . " where creator_id = :id");
         if(!$stmt) 
             throw new ApiException(500, "Failed to parse SQL statement");
 
@@ -101,7 +104,7 @@ class AbbreviationListRepository
 
     public static function load_abbr_list_by_name($conn, string $name): ?AbbreviationList
     {
-        $stmt = oci_parse($conn, "select id, creator_id, name, private, created_at, updated_at from abbr_lists where name = :name");
+        $stmt = oci_parse($conn, self::$SQL_SELECT_TEMPLATE . " where name = :name");
         if(!$stmt) 
             throw new ApiException(500, "Failed to parse SQL statement");
         
